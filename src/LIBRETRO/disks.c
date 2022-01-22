52ad35501016b71654db20e71d9f3c618b27c0c3
sylph@reset.jp <NullPopPo>
2022-01-22 10:31:16 +0000

    起動時のディスク2枚自動挿入復活

--- src/LIBRETRO/disks.c
+++ src/LIBRETRO/disks.c
@@ -119,11 +119,11 @@ void retro_disks_ready()
 #if 0
    for (i = 2; i < swap.count; i++)
       quasi88_disk_insert(DRIVE_1, retro_disks[i].filename, i - 1, 0);
+#endif
    if (swap.count > 0)
       quasi88_disk_insert(DRIVE_1, retro_disks[0].filename, 0, 0);
    if (swap.count > 1)
       quasi88_disk_insert(DRIVE_2, retro_disks[1].filename, 0, 0);
-#endif
 }
 
 void retro_disks_set(retro_environment_t cb)
